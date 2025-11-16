d61d5b584b3dfc259d5f09af07ce9a2defedf2e6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-16 13:26:46 -0300

    Feature: Import and route pages

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,21 @@
-import { Welcome } from "./Welcome.tsx"
+import React, { useState } from "react";
+
+
+import { Welcome } from "./Welcome"
+import { RoomCreation } from "./RoomCreation"
+import { JoinRoom } from "./JoinRoom"
+import { Room } from "./Room"
+
+type Routes = 'home' | 'create' | 'join' | 'room'
 
 export default function Home() {
-  return <Welcome />
+  const [route, setRoute] = useState<Routes>('home');
+  
+  switch (route) {
+    case 'home': return <Welcome/>
+    case 'create': return <RoomCreation/>
+    case 'join': return <JoinRoom/>
+    case 'room': return <Room/>
+    default: return <Welcome/>
+  }
 }
