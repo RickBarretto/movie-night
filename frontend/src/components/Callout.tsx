c491fc39e7909264ad8dd3216bb5cc8ae1315305
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-20 01:00:26 -0300

    Fix: Remove personal components
    I should us ShadCN instead.

--- frontend/src/components/Callout.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Callout({ hint, ...props}) {
-  return <section>
-    <span><b>{ hint }:</b></span>
-    &nbsp;
-    { props.children }
-  </section>
-}
--- frontend/src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Card({  ...props }) {
-  return (
-    <div className="rounded-2xl bg-slate-50 shadow-xl p-6">
-      {props.children}
-    </div>
-  );
-}
