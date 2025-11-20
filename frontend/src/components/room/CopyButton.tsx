7ce2c129c4785cff72693417a27fa2792599b609
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-20 14:57:40 -0300

    Fix: Remove unused components

--- frontend/src/components/room/CopyButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@components/Button';
-
-export function CopyButton({ text, children }) {  
-  return <Button onClick={
-    () => navigator.clipboard.writeText(text)
-  }>
-    {children}
-  </Button>;
-}
