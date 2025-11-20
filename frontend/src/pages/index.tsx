50061ecad523fab686b0dcb6087324ce5b375874
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-20 00:37:08 -0300

    Style: Update index page to use ShadCN components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,38 +1,47 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Page() {
   return (
-    <div className="max-w-xl h-screen py-10 mx-auto
-      flex flex-col justify-start gap-4"
-    >
-      <header className="prose prose-invert">
-        <h1 className="text-3xl text-center font-bold">🎬 Movie Night</h1>
-        <p className="text-lg">
-          Create a room, invite friends, 
-          vote on movies,
-          and let fate decide!.
-        </p>
-      </header>
-      <div className="mx-auto flex items-center gap-2">
-        <Link href="/create"><Button className='cursor-pointer'>Create Room</Button></Link>
-        <Link href="/join"><Button className='cursor-pointer'>Join Room</Button></Link>
-      </div>
-      <article className="prose prose-invert w-fit">
-        <h3 className="font-bold">How it Works</h3>
-        <ol>
-          <li>Create or join a room.</li>
-          <li>Everyone adds movie suggestions.</li>
-          <li>Vote with upvotes &amp; downvotes.</li>
-          <li>Host locks and draws a random winner.</li>
-          <li>Enjoy the movie!</li>
-        </ol>
-      </article>
-      <span className="my-auto"></span>
-      <footer className="prose prose-invert text-center text-sm">
-        © 2025 RickBarretto
-      </footer>
-    </div>
+    <Card className="w-full max-w-xl mx-auto my-8">
+      <CardHeader>
+        <CardTitle className="text-xl">🎬 Movie Night</CardTitle>
+        <CardDescription>
+          Create a room, invite friends, vote on movies, and let fate decide!.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <article className="w-fit">
+          <h3 className="font-bold">How it Works</h3>
+          <ol className="list-decimal pl-6">
+            <li>Create or join a room.</li>
+            <li>Everyone adds movie suggestions.</li>
+            <li>Vote with upvotes &amp; downvotes.</li>
+            <li>Host locks and draws a random winner.</li>
+            <li>Enjoy the movie!</li>
+          </ol>
+        </article>
+      </CardContent>
+      <CardFooter className="flex-col gap-5">
+        <div className="flex gap-2">
+          <Link href="/create">
+            <Button className="cursor-pointer">Create Room</Button>
+          </Link>
+          <Link href="/join">
+            <Button className="cursor-pointer">Join Room</Button>
+          </Link>
+        </div>
+        <span>© 2025 RickBarretto</span>
+      </CardFooter>
+    </Card>
   );
 }
