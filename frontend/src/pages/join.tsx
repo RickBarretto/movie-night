e297adf3ea6c93ba0a6ef0454c467defd92d509b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-20 00:58:50 -0300

    Style: Use ChadCN components on Join Page

--- frontend/src/pages/join.tsx
+++ frontend/src/pages/join.tsx
@@ -1,28 +1,41 @@
 import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
-import { TextInput } from '@/components/Input';
-import { CardForm } from '@/components/Form';
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function JoinPage() {
   return (
-    <div className="max-w-xl h-screen py-10 mx-auto 
-      flex flex-col justify-start gap-4"
-    >
-      <Link href="/">
-        <Button className="bg-transparent text-white cursor-pointer">
-          &larr; &nbsp; Back
-        </Button>
-      </Link>
-      <CardForm title="Join a Room">
-        <TextInput id="room-code" placeholder="ENTER YOUR ROOM CODE">
-          Room Code
-        </TextInput>
-        <TextInput id="username" placeholder="Enter your name">
-          Your name
-        </TextInput>
-        <Button type="submit">Join Room</Button>
-      </CardForm>
-    </div>
+    <Card className="w-full max-w-xl mx-auto my-8">
+      <CardHeader>
+        <CardTitle>Join a Room</CardTitle>
+        <CardAction>
+          <Link className="cursor-pointer" href="/">
+            <Button className="cursor-pointer">&larr; &nbsp; Back</Button>
+          </Link>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form className="flex flex-col gap-2">
+          <input name="roomCode" type="text" placeholder="Room Code" />
+          <input name="username" type="text" placeholder="Your Name" />
+          <Button name="join" type="submit" className="cursor-pointer">
+            Join Room
+          </Button>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <CardDescription>
+          Don&apos;t have a room? <Link href="/create" className='underline'>Create one</Link>
+        </CardDescription>
+      </CardFooter>
+    </Card>
   );
 }
