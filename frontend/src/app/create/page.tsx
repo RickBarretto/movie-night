e02a8dc7d7ec3743f909d19aaaf3a64c282518f7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-21 13:32:09 -0300

    Fix: Link should be a button to the homepage instead of an icon

--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -3,11 +3,12 @@
 import { useRouter } from "next/navigation";
 import { useLocalStorage } from "@uidotdev/usehooks";
 
+import Link from 'next/link';
+
 import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "lucide-react";
 
 
 export default function Page() {
