15f0207b452e4e59928fa0a64dca28acc7f69312
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-11-21 10:35:24 -0300

    Feature: Fetch room code for joining

--- frontend/src/app/join/page.tsx
+++ frontend/src/app/join/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+import { useLocalStorage } from "@uidotdev/usehooks";
+
 import Link from 'next/link';
 
 import { Button } from "@/components/ui/button";
@@ -16,6 +19,28 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function JoinPage() {
+  const baseURL = "http://localhost:3000"
+  const api = `${baseURL}/rooms`
+
+  const router = useRouter();
+  const [username, setUsername] = useLocalStorage("username");
+  const [roomCode, setRoomCode] = useLocalStorage("roomCode");
+
+  const joinRoom = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const roomCode = formData.get("roomCode") as string;
+    const username = formData.get("username") as string;
+    const response = await fetch(`${api}/${roomCode}`);
+    const data = await response.json();
+    console.log(data);
+
+    setUsername(username);
+    setRoomCode(data.room);
+
+    router.push("/room");
+  };
+
   return (
     <Card className="w-full max-w-xl mx-auto my-8">
       <CardHeader>
@@ -27,7 +52,7 @@ export default function JoinPage() {
         </CardAction>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-2">
+        <form onSubmit={joinRoom} className="flex flex-col gap-2">
           <Label htmlFor="roomCode">Room Code</Label>
           <Input name="roomCode" type="text" placeholder="CODE123" />
           <Label htmlFor="username">Your Name</Label>
